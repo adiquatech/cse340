7bfe6dd493c7bbbb3a81cb9d87ba56435f837dee
Adiquatech <quadrikolawoleadisa@gmail.com>
2025-03-22 14:45:43 +0100

    week2 learning

--- server.js
+++ server.js
@@ -10,12 +10,18 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const inventoryRoute = require("./routes/inventoryRoute")
+const utilities = require("./utilities/")
+
+/* ***********************
+ * Base COntroller
+ *************************/
+const baseController = require("./controllers/baseController")
 
 // Index route
-app.get("/", function(req, res){
-  res.render("index", {title: "Home"})
-
-})
+app.get("/", baseController.buildHome)
+// Inventory routes
+app.use("/inv", inventoryRoute)
   
 
 /* ***********************
@@ -25,10 +31,28 @@ app.set("view engine", "ejs")
 app.use(expressLayouts)
 app.set("layout", "./layouts/layout") // not at views root
 
+// File Not Found Route - must be last route in list
+app.use(async (req, res, next) => {
+  next({status: 404, message: 'Sorry, we appear to have lost that page.'})
+})
 /* ***********************
  * Routes
  *************************/
 app.use(static)
+
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message: err.message,
+    nav
+  })
+})
 
 /* ***********************
  * Local Server Information
@@ -43,3 +67,5 @@ const host = process.env.HOST
 app.listen(port, () => {
   console.log(`app listening on ${host}:${port}`)
 })
+
+
