a504796896a1164ea65645196640a6ddecb117a7
Adiquatech <quadrikolawoleadisa@gmail.com>
2025-03-25 09:42:21 +0100

    new test

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -1,9 +1,14 @@
 const utilities = require("../utilities/")
 const baseController = {}
 
-baseController.buildHome = async function(req, res){
-  const nav = await utilities.getNav()
-  res.render("index", {title: "Home", nav})
+baseController.buildHome = async function(req, res, next) {
+  try {
+    const nav = await utilities.getNav()
+    res.render("index", {title: "Home", nav}, { layout: false })
+  } catch (error) {
+    console.error("BuildHome error:", error.message)
+    next({ status: 500, message: `Home page error: ${error.message}` })
+  }
 }
 
 module.exports = baseController
