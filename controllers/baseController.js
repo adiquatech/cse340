2c4f58ce6917e65d5b64b26ae610770f5db650ed
Adiquatech <quadrikolawoleadisa@gmail.com>
2025-03-27 08:21:01 +0100

    working-test-work

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -1,10 +1,15 @@
+const utilities = require("../utilities/")
+const baseController = {}
+
 baseController.buildHome = async function(req, res, next) {
-    let nav = []
-    try {
-      nav = await utilities.getNav()
-      console.log("Nav fetched:", nav.length)
-    } catch (error) {
-      console.error("Nav error:", error.message)
-    }
-    res.render("index", {title: "Home", nav}, { layout: false })
+  let nav = []
+  try {
+    nav = await utilities.getNav()
+    console.log("Nav fetched:", nav.length)
+  } catch (error) {
+    console.error("Nav error:", error.message)
   }
+  res.render("index", {title: "Home", nav}) // Remove { layout: false }
+}
+
+module.exports = baseController
